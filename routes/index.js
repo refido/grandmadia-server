a66aa14f475e01af1f245ac7a31441b99f64e0d4
farishy <muhammadfaris9d@gmail.com>
2022-12-05 23:24:00 +0700

    api get books, books by id, post book

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
-const signupRouter = require('./signup')
-const loginRouter = require('./login')
+const signupRouter = require("./signup");
+const loginRouter = require("./login");
+const bookRoutes = require("./books");
 
-module.exports = [signupRouter, loginRouter]
+module.exports = [signupRouter, loginRouter, bookRoutes];
---
+++ schemas/books.js
@@ -0,0 +1,87 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    cover: {
+      type: String,
+      required: true,
+    },
+    newPrice: {
+      type: String,
+      required: true,
+    },
+    oldPrice: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    storeName: {
+      type: String,
+      required: true,
+    },
+    storeLocation: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    jumlahHalaman: {
+      type: String,
+      required: true,
+    },
+    tanggalTerbit: {
+      type: String,
+      required: true,
+    },
+    isbn: {
+      type: String,
+      required: true,
+    },
+    bahasa: {
+      type: String,
+      required: true,
+    },
+    penerbit: {
+      type: String,
+      required: true,
+    },
+    berat: {
+      type: String,
+      required: true,
+    },
+    lebar: {
+      type: String,
+      required: true,
+    },
+    panjang: {
+      type: String,
+      required: true,
+    },
+    information: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+bookSchema.virtual("bookId").get(function () {
+  return this._id.toHexString();
+});
+
+module.exports = mongoose.model("Books", bookSchema);
