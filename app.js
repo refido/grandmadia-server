c3f829c08593fd7ba478f2186139ec573b60ce7e
revido <revidoberliano@gmail.com>
2022-12-06 19:15:49 +0700

    cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors");
 const router = require('./routes/index')
 
 const connect = require("./schemas");
@@ -7,10 +8,12 @@ connect();
 const cookieParser = require('cookie-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', router)
 
